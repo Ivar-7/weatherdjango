6bba4f0e35e1dcfc60ed78fb42f2a1fdabf3a1c4
Ivar-7 <leank6071@gmail.com>
2024-01-25 11:31:03 +0300

    added mpesa checkout

--- weatherapp_django/urls.py
+++ weatherapp_django/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout', views.user_logout, name='logout'),
     path('register', views.register, name='register'),
     path('dashboard', views.dashboard, name='dashboard'),
+    path('mpesa', views.mpesa_payment, name='mpesa_payment'),
 ]
--- weatherapp_django/views.py
+++ weatherapp_django/views.py
@@ -6,10 +6,14 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import User
 
 from django.http import JsonResponse
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 import json
 import urllib.request
+import requests
+import datetime
+import base64
+from requests.auth import HTTPBasicAuth
 
 
 def home(request):
@@ -89,3 +93,57 @@ def view_users(request):
     # return render(request, 'auth/users.html', {'users': users})
     data = [{ 'name': user.username, 'email': user.email } for user in users ]
     return JsonResponse(data, safe=False)
+
+# Mpesa
+def mpesa_payment(request):
+    if request.method == 'POST':
+        phone = str(request.POST.get('phone'))
+        amount = str(request.POST.get('amount'))
+
+        # GENERATING THE ACCESS TOKEN
+        consumer_key = "GTWADFxIpUfDoNikNGqq1C3023evM6UH"
+        consumer_secret = "amFbAoUByPV2rM5A"
+        api_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+
+        r = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+        data = r.json()
+        access_token = "Bearer" + ' ' + data['access_token']
+
+        # GETTING THE PASSWORD
+        timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+        passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+        business_short_code = "174379"
+        data = business_short_code + passkey + timestamp
+        encoded = base64.b64encode(data.encode())
+        password = encoded.decode('utf-8')
+
+        # BODY OR PAYLOAD
+        payload = {
+            "BusinessShortCode": "174379",
+            "Password": "{}".format(password),
+            "Timestamp": "{}".format(timestamp),
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": "1",  # use 1 when testing
+            "PartyA": phone,  # change to your number
+            "PartyB": "174379",
+            "PhoneNumber": phone,
+            "CallBackURL": "https://modcom.co.ke/job/confirmation.php",
+            "AccountReference": "account",
+            "TransactionDesc": "account"
+        }
+
+        # POPULATING THE HTTP HEADER
+        headers = {
+            "Authorization": access_token,
+            "Content-Type": "application/json"
+        }
+
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"  # C2B URL
+
+        response = requests.post(url, json=payload, headers=headers)
+        print(response.text)
+
+        return HttpResponse('<h3>Please Complete Payment in Your Phone and we will deliver in minutes</h3>'
+                            '<a href="/" class="btn btn-dark btn-sm">Back to Products</a>')
+
+    return render(request, './home/mpesa.html')
